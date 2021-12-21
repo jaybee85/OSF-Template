3adf7f273167972d79254ceea4c0c48aca574631
Leigh Shayler <Leigh@arkahna.io>
2021-12-21 08:02:02 +0800

    Added missing files

---
+++ solution/Database/ADSGoFastDbUp/AdsGoFastDbUp/0-1-0-beta/A-Journaled/012-ConfigureDependencies.sql
@@ -0,0 +1,95 @@
+/*-----------------------------------------------------------------------
+
+ Copyright (c) Microsoft Corporation.
+ Licensed under the MIT license.
+
+-----------------------------------------------------------------------*/
+Update [dbo].[DataFactory]
+            Set [Name] = '$DataFactoryName$', 
+            ResourceGroup = '$ResourceGroupName$',
+            SubscriptionUid = '$SubscriptionId$',
+            DefaultKeyVaultURL = 'https://$KeyVaultName$.vault.azure.net/', 
+            LogAnalyticsWorkspaceId = '$LogAnalyticsWorkspaceId$'
+        where id = 1
+
+
+Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+            SystemServer = '$SqlServerName$.database.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Database"" : ""$SampleDatabaseName$"" }'
+        Where 
+            SystemId = '1'
+        GO
+
+        Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+            SystemServer = '$SqlServerName$.database.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Database"" : ""StagingDatabaseName$"" }'
+        Where 
+            SystemId = '2'
+        GO
+
+        Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+            SystemServer = '$SqlServerName$.database.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Database"" : ""MetadataDatabaseName$"" }'
+        Where 
+            SystemId = '11'
+        GO
+
+
+        Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+            SystemServer = 'https://$BlobStorageName$.blob.core.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Container"" : ""datalakeraw"" }'
+        Where 
+            SystemId = '3'
+        GO
+
+        Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+            SystemServer = 'https://$BlobStorageName$.blob.core.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Container"" : ""datalakelanding"" }'
+        Where 
+            SystemId = '7'
+        GO
+
+        Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+            SystemServer = 'https://$BlobStorageName$.blob.core.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Container"" : ""transientin"" }'
+        Where 
+            SystemId = '9'
+        GO
+
+        Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+            SystemServer = 'https://$AdlsStorageName$.dfs.core.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Container"" : ""datalakeraw"" }'
+        Where 
+            SystemId = '4'
+        GO
+
+        Update 
+        [dbo].[SourceAndTargetSystems]
+        Set 
+        SystemServer = 'https://$AdlsStorageName$.dfs.core.windows.net',
+            SystemKeyVaultBaseUrl = 'https://$KeyVaultName$.vault.azure.net/',
+            SystemJSON = '{ ""Container"" : ""datalakelanding"" }'
+        Where 
+            SystemId = '8'
+        GO
---
+++ solution/Database/ADSGoFastDbUp/AdsGoFastDbUp/AzureSqlServerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DbUp.Builder;
+using DbUp.SqlServer;
+
+
+
+/// <summary>Configuration extension methods for Azure SQL Server.</summary>
+// NOTE: DO NOT MOVE THIS TO A NAMESPACE
+// Since the class just contains extension methods, we leave it in the global:: namespace so that it is always available
+// ReSharper disable CheckNamespace
+public static class AzureSqlServerExtensions
+{
+
+    /// <summary>Creates an upgrader for Azure SQL Databases using Azure AD Integrated Security.</summary>
+    /// <param name="supported">Fluent helper type.</param>
+    /// <param name="connectionString">The connection string.</param>
+    /// <param name="schema">The SQL schema name to use. Defaults to 'dbo' if <see langword="null" />.</param>
+    /// <returns>A builder for a database upgrader designed for Azure SQL Server databases.</returns>
+    public static UpgradeEngineBuilder AzureSqlDatabaseWithIntegratedSecurity(this SupportedDatabases supported, string connectionString, string schema)
+    {
+        return supported.SqlDatabase(new AzureSqlConnectionManager(connectionString), schema);
+    }
+}
