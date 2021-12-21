d9d49de60e4c5abfdf64c0d37cc2c8aff4a5dfe7
Leigh Shayler <Leigh@arkahna.io>
2021-12-22 07:11:14 +0800

    Updating the AAD app registration details to use default identifierUri format and generate unique id for roles.

--- solution/DeploymentV2/terraform/app_service.tf
+++ solution/DeploymentV2/terraform/app_service.tf
@@ -1,8 +1,9 @@
 
-resource "azuread_application" "web" {
+resource "random_uuid" "app_reg_role_id" {}
+
+resource "azuread_application" "web_reg" {
   count           = var.deploy_azure_ad_web_app_registration ? 1 : 0
   display_name    = local.aad_webapp_name
-  identifier_uris = [local.webapp_identifier_uri]
   web {
     homepage_url  = local.webapp_url
     redirect_uris = ["${local.webapp_url}/signin-oidc"]
@@ -12,7 +13,7 @@ resource "azuread_application" "web" {
   }
   app_role {
     allowed_member_types = ["User"]
-    id                   = "1a089b01-782a-4f69-b087-cc0c6d4a3f7a"
+    id                   = random_uuid.app_reg_role_id.result
     description          = "Administer features of the application"
     display_name         = "Administrator"
     enabled              = true
@@ -30,7 +31,7 @@ resource "azuread_application" "web" {
 
 resource "azuread_service_principal" "web_sp" {
   count          = var.deploy_azure_ad_web_app_registration ? 1 : 0
-  application_id = azuread_application.web[0].application_id
+  application_id = azuread_application.web_reg[0].application_id
 }
 
 resource "azurerm_app_service" "web" {
@@ -54,7 +55,7 @@ resource "azurerm_app_service" "web" {
 
     AzureAdAuth__Domain   = var.domain
     AzureAdAuth__TenantId = var.tenant_id
-    AzureAdAuth__ClientId = azuread_application.web[0].application_id
+    AzureAdAuth__ClientId = azuread_application.web_reg[0].application_id
   }
 
   site_config {
