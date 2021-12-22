3a70066c71bdf88691cd5f7a2ff57306fed5937f
Dylan Smith <dylan.smith@arkahna.io>
2021-12-22 11:59:45 +0800

    Validation Checking For Subject Area Create
    Change model to allow for validation.
    Remove SubjectFormId as it was not used.

--- solution/WebApplication/WebApplication.DataAccess/Models/SubjectArea.cs
+++ solution/WebApplication/WebApplication.DataAccess/Models/SubjectArea.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace WebApplication.Models
 {
     public partial class SubjectArea
     {
         public int SubjectAreaId { get; set; }
+        [Display(Name = "Subject Area Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Area Name")]
         public string SubjectAreaName { get; set; }
+        [Display(Name = "Is Active")]
         public bool ActiveYn { get; set; }
+        [Display(Name = "Subject Area Form")]
         public int? SubjectAreaFormId { get; set; }
+        [Display(Name = "Target Json Schema")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Json Schema")]
         public string DefaultTargetSchema { get; set; }
+        [Display(Name = "Updated By")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid User")]
         public string UpdatedBy { get; set; }
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime ValidFrom { get; set; }
