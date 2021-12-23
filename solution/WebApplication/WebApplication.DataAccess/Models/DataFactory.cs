2d8cc6b3806f7c8d255e50939c6547ffdb809f26
Dylan Smith <dylan.smith@arkahna.io>
2021-12-23 14:47:10 +0800

    Validation Check DataFactory
    Created validation for DataFactory

--- solution/WebApplication/WebApplication.DataAccess/Models/DataFactory.cs
+++ solution/WebApplication/WebApplication.DataAccess/Models/DataFactory.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication.Models
 {
     public partial class DataFactory
     {
         public long Id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Name")]
         public string Name { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Resource Group")]
         public string ResourceGroup { get; set; }
         public Guid? SubscriptionUid { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid KeyVault Url")]
         public string DefaultKeyVaultUrl { get; set; }
         public Guid? LogAnalyticsWorkspaceId { get; set; }
     }
