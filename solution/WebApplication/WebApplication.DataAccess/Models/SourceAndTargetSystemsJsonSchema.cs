bddb9a9b5e5daaa703562d330767d5ce4ce2b8ae
Dylan Smith <dylan.smith@arkahna.io>
2021-12-23 14:41:31 +0800

    Validation Check SourceAndTargetJsonSchema
    Created validation for Source And Target Json Schema

--- solution/WebApplication/WebApplication.DataAccess/Models/SourceAndTargetSystemsJsonSchema.cs
+++ solution/WebApplication/WebApplication.DataAccess/Models/SourceAndTargetSystemsJsonSchema.cs
@@ -1,8 +1,14 @@
-﻿namespace WebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
 {
     public partial class SourceAndTargetSystemsJsonSchema
     {
+        [Display(Name = "Type")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Type")]
         public string SystemType { get; set; }
+        [Display(Name = "Json Schema")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Json Schema")]
         public string JsonSchema { get; set; }
     }
 }
