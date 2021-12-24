d562d124f1215369773b01e2e82331aa006877fd
Dylan Smith <dylan.smith@arkahna.io>
2021-12-24 12:18:55 +0800

    Validation Check Schedule Master
    Created validation for Schedule Master

--- solution/WebApplication/WebApplication.DataAccess/Models/ScheduleMaster.cs
+++ solution/WebApplication/WebApplication.DataAccess/Models/ScheduleMaster.cs
@@ -1,11 +1,18 @@
-﻿namespace WebApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
 {
     [System.ComponentModel.DataAnnotations.DisplayColumn("ScheduleDesciption")]
     public partial class ScheduleMaster
     {
         public long ScheduleMasterId { get; set; }    
+        [Display(Name = "Description")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Description")]
         public string ScheduleDesciption { get; set; }
+        [Display(Name = "Cron Expression")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please input valid Cron Expression")]
         public string ScheduleCronExpression { get; set; }
+        [Display(Name = "Is Active")]
         public bool ActiveYn { get; set; }
     }
 }
