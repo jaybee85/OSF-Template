8c7a9b1925e092d20995ab0b8cc3a2c79ea2ac22
John Rampono <John.Rampono@microsoft.com>
2022-04-25 11:04:13 +0800

    Upgrading to .net 6.0

--- solution/WebApplication/WebApplication/Startup.cs
+++ solution/WebApplication/WebApplication/Startup.cs
@@ -134,10 +134,10 @@ namespace WebApplication
             }
             else
             {
-                app.UseDeveloperExceptionPage();
-                //app.UseExceptionHandler("/Home/Error");
+                //app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
