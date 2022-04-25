e1443fc37365f970e91344bfd656a7102b52440e
John Rampono <john.rampono@microsoft.com>
2022-04-25 09:12:48 +0800

    Upgrading to .Net 6.0

--- solution/WebApplication/WebApplication/Services/SecurityAccessProvider.cs
+++ solution/WebApplication/WebApplication/Services/SecurityAccessProvider.cs
@@ -12,7 +12,7 @@ namespace WebApplication.Services
     public class SecurityAccessProvider : ISecurityAccessProvider
     {
         private readonly ILogger _logger;
-        private readonly IMicrosoftGraphService _graphService;
+        //private readonly IMicrosoftGraphService _graphService;
         private readonly IOptions<SecurityModelOptions> _options;
         private const string WildCardString = "*";
 
@@ -20,7 +20,7 @@ namespace WebApplication.Services
         {
             _options = options;
             _logger = logger;
-            _graphService = graphService;
+            //_graphService = graphService;
         }
 
         public bool CanPerformOperation(string controllerName, string actionName, ClaimsIdentity identity)
