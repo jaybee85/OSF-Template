5a0f3ca3d8e33ba09252bfc350ef45756222a6fc
LeonBravo <leonardo.bravo@gmail.com>
2022-06-23 21:36:02 +1000

    teacher and learningstandard

--- solution/Database/ADSGoFastDbUp/SIF/1-0-0/A-Journaled/339_vw_TeachingGroup_Teacher.sql
+++ solution/Database/ADSGoFastDbUp/SIF/1-0-0/A-Journaled/339_vw_TeachingGroup_Teacher.sql
@@ -1,34 +1,35 @@
 
+declare @path varchar(200)= 'samples/sif/TeachingGroup/TeachingGroup/Snapshot/TeachingGroup/**';
 
-DROP VIEW IF EXISTS dbo.vw_TeachingGroup_Teacher;
-GO
-
-CREATE VIEW dbo.vw_TeachingGroup_Teacher
+declare @statement varchar(max) =
+'CREATE VIEW dbo.vw_TeachingGroup_Teacher
 AS
 SELECT DISTINCT
     TG.RefId,
     TeacherList.Association,
     TeacherList.StaffPersonalRefId
-FROM OPENROWSET(
-BULK 'samples/sif/TeachingGroup/TeachingGroup/Snapshot/TeachingGroup/**',
-DATA_SOURCE ='sif_eds',
-FORMAT='PARQUET'
+FROM 
+    OPENROWSET(
+    BULK  '''+@path+''',
+	DATA_SOURCE =''sif_eds'',
+    FORMAT=''PARQUET''
 ) 
 WITH(
     RefId VARCHAR(50),
     TeacherList VARCHAR(MAX)   
 ) AS TG
-CROSS APPLY OPENJSON(TG.TeacherList,'$.TeachingGroupTeacher')
+CROSS APPLY OPENJSON(TG.TeacherList,''$.TeachingGroupTeacher'')
         WITH (
         Association VARCHAR(100),
         StaffPersonalRefId VARCHAR(50),
         StaffLocalId  VARCHAR(50),
-        [Name] NVARCHAR(MAX) '$.Name' AS JSON,
-        id int '$.sql:identity()'
-    ) as TeacherList
-;
+        [Name] NVARCHAR(MAX) ''$.Name'' AS JSON,
+        id int ''$.sql:identity()''
+    ) as TeacherList';
 
--- SELECT * FROM vw_TeachingGroup_Teacher
+execute (@statement)
+;
+GO
 
 
 
