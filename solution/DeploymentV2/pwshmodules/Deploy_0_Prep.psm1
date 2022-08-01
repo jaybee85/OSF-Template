b5cd3aeb029699dd58bf89534a41e17b5e02a6be
John Rampono <john.rampono@microsoft.com>
2022-08-01 18:02:29 +1000

    Fixed issue with CICD IP Address

--- solution/DeploymentV2/pwshmodules/Deploy_0_Prep.psm1
+++ solution/DeploymentV2/pwshmodules/Deploy_0_Prep.psm1
@@ -78,7 +78,7 @@ function PrepareDeployment (
     try
     {
         $hiddenoutput = az keyvault network-rule add -g $env:TF_VAR_resource_group_name --name $env:keyVaultName --ip-address $env:TF_VAR_ip_address/32
-        $hiddenoutput = az synapse workspace firewall-rule create --name AllowCICD --resource-group $env:TF_VAR_resource_group_name --start-ip-address $env:TF_VAR_ip_address --end-ip-address $env:TF_VAR_ip_address --workspace-name $env:ARM_SYNAPSE_WORKSPACE_NAME
+        $hiddenoutput = az synapse workspace firewall-rule create --name AllowCICD_Agent --resource-group $env:TF_VAR_resource_group_name --start-ip-address $env:TF_VAR_ip_address --end-ip-address $env:TF_VAR_ip_address --workspace-name $env:ARM_SYNAPSE_WORKSPACE_NAME
         $hiddenoutput = az storage account network-rule add --resource-group $env:TF_VAR_resource_group_name --account-name $env:datalakeName --ip-address $env:TF_VAR_ip_address
     }
     catch
@@ -89,7 +89,7 @@ function PrepareDeployment (
     try
     {
         $hiddenoutput = az keyvault network-rule add -g $env:TF_VAR_resource_group_name --name $env:keyVaultName --ip-address $env:TF_VAR_ip_address2/32
-        $hiddenoutput = az synapse workspace firewall-rule create --name AllowCICD --resource-group $env:TF_VAR_resource_group_name --start-ip-address $env:TF_VAR_ip_address2 --end-ip-address $env:TF_VAR_ip_address2 --workspace-name $env:ARM_SYNAPSE_WORKSPACE_NAME
+        $hiddenoutput = az synapse workspace firewall-rule create --name AllowCICD_User --resource-group $env:TF_VAR_resource_group_name --start-ip-address $env:TF_VAR_ip_address2 --end-ip-address $env:TF_VAR_ip_address2 --workspace-name $env:ARM_SYNAPSE_WORKSPACE_NAME
         $hiddenoutput = az storage account network-rule add --resource-group $env:TF_VAR_resource_group_name --account-name $env:datalakeName --ip-address $env:TF_VAR_ip_address2
     }
     catch
